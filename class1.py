d63cbe8ae205cd91fae25aaebf08e7665929f246
abin3007 <67953306+abin3007@users.noreply.github.com>
2020-07-07 14:05:54 +0400

    Add files via upload

---
+++ class1.py
@@ -0,0 +1,102 @@
+"""mylist=[]
+mylist.append(5)
+mylist.append(8)
+mylist.append(76)
+mylist.append(87)
+mylist.append(74)
+mylist.append(99)
+mylist.append(54)
+mylist.append(66)
+mylist.append(32)
+mylist.append(7)
+print(mylist[0]) 
+print(mylist[1])
+print(mylist[2]) 
+print(mylist[3])
+print(mylist[4])
+print(mylist[5])
+print(mylist[6])
+print(mylist[7])
+print(mylist[8])
+# animals list
+animals = ['cat', 'dog', 'rabbit', 'guinea pig']
+# 'rabbit' is removed
+
+animals.remove('rabbit')
+# Updated animals List
+
+print('Updated animals list: ', animals)
+
+cars=["ferrari","toyota","nissan","lamborghini","mercedes"]
+#abin is added
+cars.append("abin")
+cars.append(99.9)
+cars.remove("toyota")
+print("updated cars list",cars)
+
+abin=["messi","ronaldo","neymar","mbappe","pogba"]
+#ronaldo is removed
+abin.remove("ronaldo")
+print(abin)
+
+number=20+5*5/5.0
+
+
+print(number)
+
+remainder = 11 % 3
+print(remainder)
+
+squared=7**2
+cubed=2**3
+print(squared)
+print(cubed)
+
+lotsofhellos="Updated animals list"*10
+print(lotsofhellos)
+
+foods=["noodles","pizza","burger","curry","butter","spaghetti"]
+foods.append("custard")
+foods.remove("curry")
+print("updated food list",foods)
+
+myfloat=76+8*6/3.7
+print(myfloat)
+
+remainder=54%35
+print(remainder)
+
+squared=995**2
+cubed=589**3
+print(squared)
+print(cubed)
+
+lotsofspectre="spectre"*25
+print(lotsofspectre)"""
+
+x = object()
+y = object()
+
+# TODO: change this code
+x_list = [x]
+y_list = [y]
+big_list = []
+
+print("x_list contains %d objects" % len(x_list))
+print("y_list contains %d objects" % len(y_list))
+print("big_list contains %d objects" % len(big_list))
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
